f4abe8be13d8cd2fc830bd30d37fa92b70542725
Arkkeeper <arkkeeper@yandex.ru>
2021-03-19 23:28:36 +0300

    scripts

--- utils/build-apple-framework.sh
+++ utils/build-apple-framework.sh
@@ -93,27 +93,12 @@ function build_apple_framework {
 function create_universal_framework {
   cd ./destroot/Library/Frameworks || exit 1
 
-  #local platforms=("$@")
+  echo "Creating universal framework for Apple Platforms"
 
-  #echo "Creating universal framework for platforms: ${platforms[*]}"
-
-  #for i in "${!platforms[@]}"; do
-  #  platforms[$i]="${platforms[$i]}/hermes.framework/hermes"
-  #done
-
-  #lipo -create -output "${platforms[0]}" "${platforms[@]}"
-
-  # Once all was linked into a single framework, clean destroot
-  # from unused frameworks
-  #for platform in "${@:2}"; do
-  #  rm -r "$platform"
-  #done
-
-  #lipo -info "${platforms[0]}"
-
-  xcodebuild -create-xcframework -framework iphoneos/hermes.framework -framework iphonesimulator/hermes.framework -output iphoneos/hermes.xcframework
+  xcodebuild -create-xcframework -framework iphoneos/hermes.framework -framework iphonesimulator/hermes.framework -framework macosx/hermes.framework -output iphoneos/hermes.xcframework
 
   rm -r iphonesimulator
+  rm -r macosx
   rm -r iphoneos/hermes.framework
 
   cd - || exit 1
